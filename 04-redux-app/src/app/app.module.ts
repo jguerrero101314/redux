348ee72e352cc842db9e1c030315d9543ec1fe4c
joguerrero <joguerrero@bgeneral.com>
2021-07-18 12:37:01 -0500

    29. Store DevTools

--- 04-redux-app/src/app/app.module.ts
+++ 04-redux-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from '../app/contador/contador.reducer';
+import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
@@ -13,6 +15,11 @@ import { NietoComponent } from './contador/nieto/nieto.component';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ count: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
