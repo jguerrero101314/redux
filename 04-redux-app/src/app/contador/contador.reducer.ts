56766778c98afea5e2bae5495c7050746f02c7c0
joguerrero <joguerrero@bgeneral.com>
2021-07-19 13:54:05 -0500

    31. Create Reducer

--- 04-redux-app/src/app/contador/contador.reducer.ts
+++ 04-redux-app/src/app/contador/contador.reducer.ts
@@ -1,15 +1,25 @@
-import { Action } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 import { decrement, increment } from './contador.actions';
+export const initialState = 0;
 
-export const contadorReducer = (state: number = 10, action: Action) => {
-  switch (action.type) {
-    case increment.type:
-      return state + 1;
+const _counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1)
+);
 
-    case decrement.type:
-      return state - 1;
+export function contadorReducer(state, action: Action) {
+  return _counterReducer(state, action);
+}
 
-    default:
-      return state;
-  }
-};
+// export const contadorReducer = (state: number = 10, action: Action) => {
+//   switch (action.type) {
+//     case increment.type:
+//       return state + 1;
+//     case decrement.type:
+//       return state - 1;
+
+//     default:
+//       return state;
+//   }
+// };
