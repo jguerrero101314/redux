1fd75e9ba6cff469ee021de4db79ea4632fc5082
joguerrero <joguerrero@bgeneral.com>
2021-07-25 13:34:04 -0500

    42. Instalar NgRx, modelo, acciones y reducer de Todos

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
-import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { TodoModule } from './todos/todo.module';
+import { todoReducer } from './todos/todo.reducer';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
-  imports: [BrowserModule, TodoModule],
+  imports: [
+    BrowserModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
