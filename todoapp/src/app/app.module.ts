90d05e1be65d0360bd7984f79217ae9d9645a4e8
joguerrero <joguerrero@bgeneral.com>
2021-07-25 13:43:52 -0500

    43. Definiendo el AppState de nuestra aplicación

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { TodoModule } from './todos/todo.module';
@@ -12,6 +14,11 @@ import { todoReducer } from './todos/todo.reducer';
     BrowserModule,
     TodoModule,
     StoreModule.forRoot({ todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
