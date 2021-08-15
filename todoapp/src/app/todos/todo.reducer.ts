fa23d7df8bc66cdb42f3c6aee42c4030a286e229
joguerrero <joguerrero@bgeneral.com>
2021-08-15 17:30:04 -0500

    46. Controlar los TODO-Items

--- todoapp/src/app/todos/todo.reducer.ts
+++ todoapp/src/app/todos/todo.reducer.ts
@@ -2,7 +2,13 @@ import { createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo.model';
 import { add } from './todo.actions';
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [
+  new Todo('Salvar al mundo'),
+  new Todo('Vencer a thanos'),
+  new Todo('Robar escudo'),
+  new Todo('comer'),
+  new Todo('hola'),
+];
 
 const _todoReducer = createReducer(
   initialState,
