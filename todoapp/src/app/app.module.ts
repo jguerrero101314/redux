f65fa924a3886298602348b9cf002804f0acc053
joguerrero <joguerrero@bgeneral.com>
2021-09-04 14:34:35 -0500

    52. Combinando 2 o más reducers en el AppState

--- todoapp/src/app/app.module.ts
+++ todoapp/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
+import { appReducers } from './app.reducer';
 import { FooterComponent } from './footer/footer.component';
 import { TodoModule } from './todos/todo.module';
-import { todoReducer } from './todos/todo.reducer';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
     TodoModule,
-    StoreModule.forRoot({ todos: todoReducer }),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
