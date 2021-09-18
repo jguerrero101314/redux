9f871935c574d14398941cd981eb3a08c9043ec4
joguerrero <joguerrero@bgeneral.com>
2021-09-18 12:35:18 -0500

    [200~72. Pantalla de registro~

--- redux-ingreso-egreso-1.0.0/src/app/app.module.ts
+++ redux-ingreso-egreso-1.0.0/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 // Modulos
-import { AppRoutingModule } from './app-routing.module';
-
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
-import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { DetalleComponent } from "./ingreso-egreso/detalle/detalle.component";
+import { EstadisticaComponent } from "./ingreso-egreso/estadistica/estadistica.component";
+import { IngresoEgresoComponent } from "./ingreso-egreso/ingreso-egreso.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
 
 @NgModule({
   declarations: [
@@ -28,13 +25,10 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     DetalleComponent,
     FooterComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- redux-ingreso-egreso-1.0.0/src/app/auth/register/register.component.ts
+++ redux-ingreso-egreso-1.0.0/src/app/auth/register/register.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styles: []
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
+  registroForm: FormGroup;
 
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(64),
+        ],
+      ],
+      email: ["", [Validators.required, Validators.email]],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(64),
+        ],
+      ],
+    });
   }
 
+  crearUsuario() {
+    console.log(this.registroForm);
+    console.log(this.registroForm.valid);
+    console.log(this.registroForm.value);
+  }
 }
