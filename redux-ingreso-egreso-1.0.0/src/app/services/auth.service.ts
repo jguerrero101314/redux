94bed527f688c4a7928d6eaf513c9658a126bd37
joguerrero <joguerrero@bgeneral.com>
2021-09-26 11:13:05 -0500

    79. Obtener información del usuario activo en Firebase

--- redux-ingreso-egreso-1.0.0/src/app/services/auth.service.ts
+++ redux-ingreso-egreso-1.0.0/src/app/services/auth.service.ts
@@ -7,6 +7,14 @@ import { AngularFireAuth } from "@angular/fire/auth";
 export class AuthService {
   constructor(private readonly auth: AngularFireAuth) {}
 
+  initAuthListener() {
+    this.auth.authState.subscribe((fuser) => {
+      console.log(fuser);
+      console.log(fuser?.uid);
+      console.log(fuser?.email);
+    });
+  }
+
   crearUsuario(email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
